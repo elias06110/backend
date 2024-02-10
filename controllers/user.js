8a2000205a5f1e7a0ea8e70fe6629b056276bd23
elias06110 <elias06110@gmail.com>
2024-02-10 17:47:51 +0100

    correction

--- controllers/user.js
+++ controllers/user.js
@@ -12,7 +12,7 @@ exports.signup = (req, res, next) => {
             password: hash
         })
         user.save()
-        .then(()=>res.status(201).json({mesage:'Utilisateur créé !'}))
+        .then(()=>res.status(201).json({message:'Utilisateur créé !'}))
         .catch(error=>res.status(400).json({error}))
     })
     .catch(error=>res.status(500).json({error}));
